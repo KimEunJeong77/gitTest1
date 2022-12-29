487159f85e1c9846914b4b0ea2f2ac16a01c93c6
KimJAVA <cheritis@naver.com>
2022-12-29 15:34:14 +0900

    project생성

---
+++ src/test/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test1")
+	public String test1() {
+		return "test1";
+	}
+}
